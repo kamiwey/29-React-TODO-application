f4129efcea845e474cb4f83464d1ec0edee03143
kamiwey <kamiwey@gmail.com>
2022-11-18 14:22:29 +0100

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,44 +3,44 @@ import React, { useState } from "react";
 //create your first component
 export function Home() {
 	
-	const [tasklist, setTaskList] = useState([]);
-	const [task, setTask] = useState("");
-	const [hoverTask, setHoverTask] = useState(false);
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [mouseHover, setMouseHover] = useState(false);
 
 	
-	const HandleOnKeyPress = e => {
+	const keyPress = e => {
 		
-		if (e.key === "Enter" && task !== "") {
+		if (e.key === "Enter" && inputValue !== "") {
 			e.preventDefault();
 			
 			const newTask = {
 				id: new Date().getTime(),
-				description: task
+				description: inputValue
 			};
 			
-			setTaskList([...tasklist].concat(newTask));
+			setTodos([...todos].concat(newTask));
 			
-			setTask("");
-		} else if (e.key === "Enter" && task == "") {
+			setInputValue("");
+		} else if (e.key === "Enter" && inputValue == "") {
 			alert("The input cannot be empty");
 		}
 	};
 
-	const GenList = () => {
+	const GetTaskList = () => {
 		
-		return tasklist.map(task => (
+		return todos.map(inputValue => (
 			<li
-				key={task.id}
+				key={inputValue.id}
 				className="list-group-item"
-				onMouseEnter={() => setHoverTask(task.id)}>
+				onMouseEnter={() => setMouseHover(inputValue.id)}>
 				<p className="d-inline-block text-secondary ml-4 fs-3 align-middle ">
-					-{task.description}
+					-{inputValue.description}
 				</p>
-				{task.id == hoverTask ? (
+				{inputValue.id == mouseHover ? (
 					<button
 						type="button"
 						className="delete btn text-muted"
-						onClick={() => DeleteTask(task.id)}>
+						onClick={() => DeleteTodo(inputValue.id)}>
 						<i className="fa-regular fa-trash-can"></i>
 					</button>
 				) : null}
@@ -49,9 +49,9 @@ export function Home() {
 	};
 
 	
-	const DeleteTask = id => {
-		const updateTaskList = [...tasklist].filter(task => task.id !== id);
-		setTaskList(updateTaskList);
+	const DeleteTodo = id => {
+		const updateTaskList = [...todos].filter(inputValue => inputValue.id !== id);
+		setTodos(updateTaskList);
 	};
 
 	
@@ -61,23 +61,23 @@ export function Home() {
 			<div className="tasker">
 			<input
 				type="text"
-				placeholder="Type a new task"
+				placeholder="Type a new inputValue"
 				className="tasker2 text-muted"
-				value={task}
-				onChange={e => setTask(e.target.value)}
-				onKeyPress={e => HandleOnKeyPress(e)}
+				value={inputValue}
+				onChange={e => setInputValue(e.target.value)}
+				onKeyPress={e => keyPress(e)}
 			>
 
 			</input>
 			</div>
 			<ul className="list-group">
-				{GenList()}
+				{GetTaskList()}
 				<div>
 					<label htmlFor="list-group-item">
 						<p className="text-muted ml-5 mt-2">
-							{tasklist.length == 0
-								? " No tasks, add a task"
-								: tasklist.length + " item left"}
+							{todos.length == 0
+								? " No tasks, add a inputValue"
+								: todos.length + " item left"}
 						</p>
 					</label>
 				</div>
